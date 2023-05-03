0e3953f8cf5c7a0f12c85c6916c3d680d39b0f74
DoyeonKwak <21746490-DoyeonKwak@users.noreply.replit.com>
2023-05-03 13:18:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const video = document.getElementById("video");
+const captureBtn = document.getElementById("capture-btn");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const thumbnails = document.getElementById("thumbnails");
+
+const constraints = {
+  video: {
+    width: { exact: 240 },
+    height: { exact: 320 }
+  }
+};
+
+let imgCounter = 0;
+
+async function initCamera() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    video.srcObject = stream;
+  } catch (err) {
+    console.error("Error accessing the camera", err);
+  }
+}
+
+function captureImage() {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+
+  const img = new Image();
+  img.src = canvas.toDataURL("image/png");
+
+  if (imgCounter >= 4) {
+    imgCounter = 0;
+  }
+  let existingWrapper = thumbnails.querySelector(`#wrapper${imgCounter}`);
+  if (existingWrapper) {
+    let existingImg = existingWrapper.querySelector("img");
+    existingImg.src = img.src;
+  } else {
+    img.id = `thumb${imgCounter}`;
+
+    const wrapper = document.createElement("div");
+    wrapper.id = `wrapper${imgCounter}`;
+    wrapper.className = "thumbnail-wrapper";
+    wrapper.appendChild(img);
+
+    thumbnails.appendChild(wrapper);
+  }
+  imgCounter++;
+}
+
+captureBtn.addEventListener("click", captureImage);
+
+initCamera();
